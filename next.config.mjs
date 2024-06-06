94a1723d7e416eb5d1eef5ebee86808abc6b218b
Lovely Jaca <jlablab@gmail.com>
2024-06-06 16:15:02 +0800

    env

--- next.config.mjs
+++ next.config.mjs
@@ -9,8 +9,7 @@ const nextConfig = {
   //     '/': { page: '/' },
   //   };
   // },
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
+  
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -23,6 +22,12 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
+  env: {
+    NEXT_PUBLIC_AIRTABLE_API_KEY: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+    NEXT_PUBLIC_AIRTABLE_BASE_ID: process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID,
+  },
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
