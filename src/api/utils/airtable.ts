f4f5b9cca6d3acee9dec06a110d7f29eadaebb56
Lovely Jaca <jlablab@gmail.com>
2024-06-06 17:43:36 +0800

    test

--- src/api/utils/airtable.ts
+++ src/api/utils/airtable.ts
@@ -1,9 +1,13 @@
 
 import Airtable from "airtable";
 
+// const base = new Airtable({
+//   apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
+// }).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
+
 const base = new Airtable({
-  apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
-}).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
+  apiKey: "patHiEnS7JS9vLoxo.ab40ba520cec5c3ce19b66f7f038dee76570428e4c740f21723e7420580fa41e",
+}).base("app8N3c1YciCZrGlJ");
 
 const minifyRecord = (record:any) => {
   return {
