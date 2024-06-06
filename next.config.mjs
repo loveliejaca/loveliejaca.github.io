831576f3f56c8f1d0e39de3a992928e49178c11a
Lovely Jaca <jlablab@gmail.com>
2024-06-06 17:02:28 +0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ const nextConfig = {
 	sassOptions: {
     includePaths: ['styles'],
   },
-  exportTrailingSlash: true,
-  exportPathMap: async () => {
-    return {
-      '/': { page: '/' },
-    };
-  },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
