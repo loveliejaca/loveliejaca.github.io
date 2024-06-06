e58fed82adbba994e936e8407d8e5ef6f49c6df3
Lovely Jaca <jlablab@gmail.com>
2024-06-06 16:43:05 +0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,13 @@ const nextConfig = {
 	sassOptions: {
     includePaths: ['styles'],
   },
-  // exportTrailingSlash: true,
-  // exportPathMap: async () => {
-  //   return {
-  //     '/': { page: '/' },
-  //   };
-  // },
+  exportTrailingSlash: true,
+  exportPathMap: async () => {
+    return {
+      '/': { page: '/' },
+    };
+  },
   output: "export",  // <=== enables static exports
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
   reactStrictMode: true,
 };
 
