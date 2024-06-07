d7ca97f58a6b983c5591cc47c386341523a48bd1
Lovely Jaca <jlablab@gmail.com>
2024-06-07 15:55:27 +0800

    env

--- src/api/utils/airtable.ts
+++ src/api/utils/airtable.ts
@@ -1,14 +1,18 @@
 
-import { baseID, dbAPI } from "@/helper/constant";
+import { dbAPI, baseID } from "@/helper/constant";
 import Airtable from "airtable";
 
 // const base = new Airtable({
 //   apiKey: process.env.NEXT_PUBLIC_AIRTABLE_API_KEY,
 // }).base(process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID);
 
+// const base = new Airtable({
+//   apiKey: dbAPI,
+// }).base(baseID);
+
 const base = new Airtable({
-  apiKey: dbAPI,
-}).base(baseID);
+  apiKey: "patHiEnS7JS9vLoxo.ab40ba520cec5c3ce19b66f7f038dee76570428e4c740f21723e7420580fa41e",
+}).base("app8N3c1YciCZrGlJ");
 
 const minifyRecord = (record:any) => {
   return {
@@ -18,7 +22,7 @@ const minifyRecord = (record:any) => {
 };
 
 const getMinifiedRecords = (records:any) => {
-  console.log('process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID', process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID)
+  console.log('dbAPI', dbAPI, baseID)
   return records.map((record:any) => minifyRecord(record));
 };
 
