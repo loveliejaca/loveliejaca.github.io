03e67da9ea78088390f7fb29130535b97eed84b4
Lovely Jaca <jlablab@gmail.com>
2024-06-19 16:50:11 +0800

    lint error

--- src/components/modules/Skills/Skills.tsx
+++ src/components/modules/Skills/Skills.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React, { FC } from 'react';
-import { gsap } from 'gsap';
-import { useGSAP } from '@gsap/react';
 
 import styles from './Skills.module.scss';
 import Heading from '../../elements/Heading/Heading';
